f690045f45bd87a23ba3b56d716234cf2264b8d7
adamtlee <adam.lee346@gmail.com>
2025-11-11 14:25:49 -0700

    First commit to my Integrating With HubSpot I: Foundations practicum repository.

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Added comment for first commit "First commit to my Integrating With HubSpot I: Foundations practicum repository."
+
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_TOKEN;
 
