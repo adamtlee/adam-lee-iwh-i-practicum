2db62959e8f071de201dd4a53ac3941d02f172cb
adamtlee <adam.lee346@gmail.com>
2025-11-11 13:28:29 -0700

    Added env configuration to pull private app access token through environment variable

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,7 +9,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_TOKEN;
+
+// Check if environment variable is loaded
+if (PRIVATE_APP_ACCESS) {
+    console.log('PRIVATE_APP_TOKEN loaded successfully');
+} else {
+    console.warn('WARNING: PRIVATE_APP_TOKEN is not set. Please check your .env file.');
+}
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
